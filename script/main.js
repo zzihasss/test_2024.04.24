4bf9b3bd12a532c9f0ef1d33293e71c512c0ba83
zzihasss <ayyygirl12@naver.com>
2024-04-24 17:50:10 +0900

    햄버거 버튼 클릭시 좌측에서 메뉴바 호출함

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ let btnCall = document.querySelector('.btnCall');
 let menuMo = document.querySelector('.menuMo');
 
 btnCall.addEventListener('click', (e) => {
+	// e 꼭 쓰기, e = event
 	e.preventDefault();
 	btnCall.classList.toggle('on');
 	menuMo.classList.toggle('on');
